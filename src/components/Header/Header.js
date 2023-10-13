5bd9f22a64ad3e1d52f8191109cbd38fab393a66
Isabella <isabellaotoo25@gmail.com>
2023-10-13 21:42:49 +0000

    Update header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 function Header() {
   return (
     <div className={styles.container}>
-      <p className= {styles.heading}>A <span>Resume</span> that let's you stand!</p>
+      <p className= {styles.heading}>Build a <span>Resume</span> that let's you stand!</p>
       <p className= {styles.heading}>Make your own Resume <span>It's free</span></p>
     </div>
   );
